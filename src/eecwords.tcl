433a02769f2ca29e8bed77edb4a28f6277b25345
Marty McGowan <mcgowan@alum.mit.edu>
2012-07-08 21:52:55 -0400

    added avoidlist

--- src/eecwords.tcl
+++ src/eecwords.tcl
@@ -15,3 +15,19 @@ proc eec_wordlist {} {  return {wordlist
 , set
 , subtract
 } }
+proc eec_avoidlist {} {  return {avoidlist 
+any
+avoidlist
+biop
+does
+info
+init
+machinit
+one
+parse
+remain
+three
+two
+wordlist
+zero
+} }
