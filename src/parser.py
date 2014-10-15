482bc2faa376217acbb42b432f0ee7cb88223f10
Marty McGowan <mcgowan@alum.mit.edu>
2014-10-15 14:49:28 -0400

    added print, printf commands; io handler

--- src/parser.py
+++ src/parser.py
@@ -124,6 +124,12 @@ def evaluation( cmmd, nargs):
     """handler stub"""
 
 #
+# ------------------------------------------- I	--
+#
+def io( cmmd, nargs):
+    """handler stub"""
+
+#
 # ------------------------------------------- L	--
 #
 def localargs( cmmd, nargs):
@@ -143,41 +149,45 @@ def localexec( cmmd, nargs):
 #     n,m    n, ..., m
 #       n,   n, n+1, ...
 #
-#                         #args     handler       characteristics
+
 
 handler = {
-
+#     Name          handler
+    'boolean'    : boolean,
+    'collection' : collection,
+    'decision'   : decision,
     'definition' : definition,
+    'empty'      : empty,
+    'evaluation' : evaluation,
+    'io'         : io,
     'localargs'  : localargs,
     'localexec'  : localexec,
-    'boolean'    : boolean,
-    'decision'   : decision,
-    'collection' : collection,
-    'evaluation' : evaluation,
-    'empty'      : empty
+    ''           : None
 }
 
 cummings = {
-
-    'constant'         : [ [2],   'definition'], # immutable
-    'variable'         : [ [2],   'definition'], # 
-    'function'         : [ [2,3], 'definition'], # name[, args], return
+#                         #args  handlerName       characteristics
     'args'             : [ [0,],  'localargs'],  # variable number
-    'return'           : [ [0,],  'localexec'],  # stmt, stmt, ...
-    'equal'            : [ [2],   'boolean'],    # a == b
-    'not equal'        : [ [2],   'boolean'],    # a != b, string or number
-    'less than'        : [ [2],   'boolean'],    # a < b
-    'greater than'     : [ [2],   'boolean'],    # a > b
-    'greater or equal' : [ [2],   'boolean'],    # a >= b: 
-    'less or equal'    : [ [2],   'boolean'],    # a <= b: 
-    'if'               : [ [2,3], 'decision'],   # boolean ,iftrue[, else]
-    'while'            : [ [2],   'decision'],   # boolean, codeblock, e.g. return
-    'for'              : [ [3],   'decision'],   # arg, list, body, .. return
     'assert'           : [ [2,4,],'decision'],   # boolean, false message, ...
-    'list'             : [ [0,],  'collection'], # itema, itemb, ...
-    'expr'             : [ [1],   'evaluation'], # arithmatic expression
-    'concatenate'      : [ [2,],  'evaluation'], # strings, .. a, b, c,
     'comment'          : [ [0,],  'empty'],      # gobble input
+    'concatenate'      : [ [2,],  'evaluation'], # strings, .. a, b, c,
+    'constant'         : [ [2],   'definition'], # immutable
+    'equal'            : [ [2],   'boolean'],    # a == b
+    'expr'             : [ [1],   'evaluation'], # arithmatic expression
+    'for'              : [ [3],   'decision'],   # arg, list, body, .. return
+    'function'         : [ [2,3], 'definition'], # name[, args], return
+    'greater or equal' : [ [2],   'boolean'],    # a >= b: 
+    'greater than'     : [ [2],   'boolean'],    # a > b
+    'if'               : [ [2,3], 'decision'],   # boolean ,iftrue[, else]
+    'less or equal'    : [ [2],   'boolean'],    # a <= b: 
+    'less than'        : [ [2],   'boolean'],    # a < b
+    'list'             : [ [0,],  'collection'], # itema, itemb, ...
+    'not equal'        : [ [2],   'boolean'],    # a != b, string or number
+    'print'            : [ [0,2], 'io'],         # assumes stdout, string, newline
+    'printf'           : [ [1,],  'io'],         # format, or filehandle, format, arg, ...
+    'return'           : [ [0,],  'localexec'],  # stmt, stmt, ...
+    'variable'         : [ [2],   'definition'], # 
+    'while'            : [ [2],   'decision'],   # boolean, codeblock, e.g. return
     ''                 : [ [0],   'empty']       # end of list place-holder
     }
 
