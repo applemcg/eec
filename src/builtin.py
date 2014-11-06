dc1dc905a37d4a20ea96273e111c6663d73c36d1
Marty McGowan <mcgowan@alum.mit.edu>
2014-11-06 17:08:50 -0500

    classes eetoken, builtin,

--- src/builtin.py
+++ src/builtin.py
@@ -10,8 +10,8 @@ import machine
 def toStderr( msg):   sys.stderr.write(msg + '\n')
 
 #
-# Here are the builtin cummings definitions and their 
-# code handlers.
+# ------------------------- builtin cummings definitions, code handlers	--
+#
 
 
 def boolean( cmmd, nargs):
@@ -28,35 +28,7 @@ def collection( cmmd, nargs):
                 
 def decision( cmmd, nargs):
     """handler stub"""
-                    
-def definition_class(name, *args):
-    """handler stub"""
-                        
-def definition_constant(name, *args):
-    """handler stub"""
-     # EDIT MARK -- add constant definition
-                        
-def definition_variable(name, *args):
-    """handler stub"""
-                            
-def definition_function(name, *args):
-    """handler stub"""
-                                
-definition_actions = {
-    'class'      : definition_class,
-    'constant'   : definition_constant,
-    'variable'   : definition_variable,
-    'function'   : definition_function
-}
-                                
-def definition( cmmd, nargs):
-    """handler stub"""
-    toStderr( str(cmmd))
-    try:
-        definition_actions[cmmd]( nargs)
-    except KeyError:
-        print 'Unavailable Definition Action: ', cmmd
-        
+#
 def empty( cmmd, nargs):
     """handler stub"""
         
@@ -99,65 +71,174 @@ def localexec( cmmd, nargs):
 def machine( cmmd, nargs):
     """handler stub"""
 
-handler = {
-    #     Name          handler
-    'boolean'    : boolean,
-    'comment'    : comment,
-    'collection' : collection,
-    'decision'   : decision,
-    'definition' : definition,
-    'empty'      : empty,
-    'evaluation' : evaluation,
-    'io'         : io,
-    'localargs'  : localargs,
-    'localexec'  : localexec,
-    'machine'    : machine,
-    ''           : None
+#
+# -------------------------------------------- working handler template	--
+#
+class ee_constant(object):
+
+    def __init__(self, name):
+        self.name  = name
+        self.value = ''
+
+    def assign(self, value):
+        
+        
+    
+def defining_class(name, *args):
+    """handler stub"""
+
+def executing_constant():                        
+    
+
+def defining_constant(name, *args):
+    """handler stub"""
+     # EDIT MARK -- add constant definition
+                        
+def defining_variable(name, *args):
+    """handler stub"""
+                            
+def defining_function(name, *args):
+    """handler stub"""
+                                
+defining_actions = {
+    'class'      : defining_class,
+    'constant'   : defining_constant,
+    'variable'   : defining_variable,
+    'function'   : defining_function
 }
-# formerly cummings, now "token"
+                                
+def defining( cmmd, nargs):
+    """handler stub"""
+    toStderr( str(cmmd))
+    try:
+        defining_actions[cmmd](cummingstate, nargs)
+    except KeyError:
+        print 'Unavailable Definition Action: ', cmmd
+#
+# . . .+. . . .|. . . .+. . . .*. . . .+. . . .|. . . .+. . . .*. . . \
+# --------------------------------------------- handler characteristics	--
+# 
+# * immediate -- like a comment, inside a definition or not, it
+#   executes on the closing right paren
+# 
+# * defining -- it reserves memory for its instructions a/o data.
+# 
+# * bystate -- think of the global block as a compiled thread, laying down
+#   code which is executed as if it were a comment. implies a STATE of the
+#   interpreter.
+#
+#
+
+interpreting = 1
+compiling    = 0
+cummingstate = interpreting
+
+class builtin(object):
+    """a builtin shares a state-aware method for its
+    member words as builtin tokens in the language.
+    a builtin supplies an argument handling specification
+    and it's own method for state-dependent evalution.
+    """
+
+    def __init_(self)
+        self.method = method
+        self.builtin = {} 
+
+    def insert(self, token, args, handler):
+        self.builtin[token] = [args, handler]
+
+# . . .+. . . .|. . . .+. . . .*. . . .+. . . .|. . . .+. . . .*. . . \
+
+# -------------------------------------------- cummings language tokens	--
+#
 token = {
-    #                      #args     handlerName      characteristics
-    #                      --------  ------------     --------------------------
-    'args'             : [ [0,'*'],  'localargs'],  # variable number
-    'assert'           : [ [2,4,'*'],'decision'],   # boolean, false message, ...
-    'class'            : [ [2,3],    'definition'], # name [, class list], members
-    'comment'          : [ [0,'*'],  'comment'],    # gobble input
-    'concatenate'      : [ [1,'*'],  'evaluation'], # strings, .. a, b, c,
-    'constant'         : [ [2],      'definition'], # name, immutable
-    'equal'            : [ [2],      'boolean'],    # a == b
-    'expr'             : [ [1],      'evaluation'], # arithmatic expression
-    'for'              : [ [3],      'decision'],   # arg, list, body, .. return
-    'function'         : [ [2,3],    'definition'], # name [, args], return
-    'greater or equal' : [ [2],      'boolean'],    # a >= b: 
-    'greater than'     : [ [2],      'boolean'],    # a > b
-    'if'               : [ [2,3],    'decision'],   # boolean ,iftrue[, else]
-    'interpreter'      : [ [0],      'machine'],    # boolean ,iftrue[, else]
-    'less or equal'    : [ [2],      'boolean'],    # a <= b: 
-    'less than'        : [ [2],      'boolean'],    # a < b
-    'list'             : [ [0,'*'],  'collection'], # itema, itemb, ...
-    'not equal'        : [ [2],      'boolean'],    # a != b, string or number
-    'print'            : [ [0,2],    'io'],         # assumes stdout, string, newline
-    'printf'           : [ [0,'*'],  'io'],         # format, or filehandle, format, arg, ...
-    'return'           : [ [0,'*'],  'localexec'],  # stmt, stmt, ...
-    'variable'         : [ [2],      'definition'], # name, value
-    'while'            : [ [2],      'decision'],   # boolean, codeblock, e.g. return
-    ''                 : [ [0],      'empty']       # end of list place-holder
+    #                      #args     handlerName     characteristics
+    #                      --------  ------------    --------------------------
+    'args'             : [ [0,'*'],  'bystate'],   # variable numberl
+    'assert'           : [ [2,4,'*'],'bystate'],   # boolean, false message, ...
+    'class'            : [ [2,3],    'defining'],  # name [, class list], members
+    'comment'          : [ [0,'*'],  'immediate'], # gobble input
+    'concatenate'      : [ [1,'*'],  'bystate'],   # strings, .. a, b, c,
+    'constant'         : [ [2],      'defining'],  # name, immutable
+    'equal'            : [ [2],      'bystate'],   # a == b
+    'expr'             : [ [1],      'bystate'],   # arithmatic expression
+    'for'              : [ [3],      'bystate'],   # arg, list, body, .. return
+    'function'         : [ [2,3],    'defining'],  # name [, args], return
+    'greater or equal' : [ [2],      'bystate'],   # a >= b: 
+    'greater than'     : [ [2],      'bystate'],   # a > b
+    'if'               : [ [2,3],    'bystate'],   # boolean ,iftrue[, else]
+    'interpreter'      : [ [0],      'immediate'], # boolean ,iftrue[, else]
+    'less or equal'    : [ [2],      'bystate'],   # a <= b: 
+    'less than'        : [ [2],      'bystate'],   # a < b
+    'list'             : [ [0,'*'],  'bystate'],   # itema, itemb, ...
+    'not equal'        : [ [2],      'bystate'],   # a != b, string or number
+    'print'            : [ [0,2],    'bystate'],   # assumes stdout, string, newline
+    'printf'           : [ [0,'*'],  'bystate'],   # format, or filehandle, format, arg, ...
+    'return'           : [ [0,'*'],  'bystate'],   # stmt, stmt, ...
+    'variable'         : [ [2],      'defining'],  # name, value
+    'while'            : [ [2],      'bystate'],   # boolean, codeblock, e.g. return
+    ''                 : [ [0],      'bystate']    # end of list place-holder
 }
+
+def eetoken(Object):
+    """Forth has 'words', and it seems appropriate to call
+    the cummings equivalent a 'token', i.e. the elementary
+    execution token of the language.  builtins, constants,
+    variables, functions, and classes"""
+
+    def __init__(self, name, type)
+    	self.name = name
+        self.type = type
+
+    def getType(self):
+        return self.type
+
+    def getName(self):
+        return self.name
+
+def storage(eetoken):
+    """either a constant, variable or list"""
+    
+    def set(self, value):
+        self.value = value
+
+    def get(self)
+	return self.value
+
+def builtin(eetoken):
+    
+    def property(self, args, handler):
+        self.args    = args
+        self.nandler = handler
+    
+def constant(storage):
+
+    def __init__(self, name, type, value):
+        me = storage(self, name, type)
+        me.value = value
+
+    def set(self, value):
+        self.value = self.value
+    
+
+
+ 
 def eecBuiltin ():
 
     import node
     print dir(node)
 
 
-    vocabulary = []
+    vocabulary = {}
 
     for name, value in token.iteritems():
         args  = value[0]
         hnam  = value[1]
         hdlr  = handler[hnam]
         print   name + ' ' + str(args) + ' ' + hnam + ' ' +  str(hdlr)
-        vnod  = node.Node(name, hdlr, args, None, 0)
-        print   str(vnod)
-        vocabulary.append( vnod)
+
+        vnod  = builtin(name, 'builtin')
+        vnod.property( args, hdlr)
+        vocabulary[name] = vnod
         
     return vocabulary
